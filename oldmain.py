1df5a3d2b586169de3c0f870650a271f468bdf20
Dragonite <meaquaeric@gmail.com>
2023-12-08 23:06:27 +0900

    test

--- oldmain.py
+++ oldmain.py
@@ -6,25 +6,56 @@ from dataset import ChromosomeDataset
 import copy
 import numpy as np
 import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader,random_split
 
 
 # Assuming your model is defined as 'model'
 # Assuming your data loaders are defined as 'train_loader' and 'val_loader'
 
 # Define the loss function and optimizer
+feature_matrix = torch.load('/content/drive/MyDrive/jokedata/feature_matrix.pt')
+contact_matrix = torch.load('/content/drive/MyDrive/jokedata/contact_matrix.pt')
 
-data_dir = '/content/drive/MyDrive/corigamidata'
-window = 16
-length = 128
-val_chr = 1
-feature='DNA'
-itype='Outward'
-batch_size=64
+# Step 2: Create a custom dataset
+class MyDataset(Dataset):
+    def __init__(self, features, labels):
+        self.features = features
+        self.labels = labels
 
-train_dataset = ChromosomeDataset(data_dir, window, length,val_chr,feature=feature,itype=itype,mode='train')
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True,drop_last=True,num_workers=8)
-val_dataset=ChromosomeDataset(data_dir, window, length,val_chr,feature=feature,itype=itype,mode='val')
-val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False,drop_last=True,num_workers=8)
+    def __len__(self):
+        return len(self.features)
+
+    def __getitem__(self, idx):
+        return self.features[idx], self.labels[idx]
+
+dataset = MyDataset(feature_matrix, contact_matrix)
+
+# Step 3: Split the data
+# Define the proportion for the training set (e.g., 80%)
+train_size = int(0.8 * len(dataset))
+val_size = len(dataset) - train_size
+
+# Randomly split the dataset into training and validation sets
+train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+# Step 4: Create DataLoaders
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True,drop_last=True)
+val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False,drop_last=True)
+
+
+# data_dir = '/content/drive/MyDrive/corigamidata'
+# window = 16
+# length = 128
+# val_chr = 1
+# feature='DNA'
+# itype='Outward'
+# batch_size=64
+
+# train_dataset = ChromosomeDataset(data_dir, window, length,val_chr,feature=feature,itype=itype,mode='train')
+# train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True,drop_last=True,num_workers=8)
+# val_dataset=ChromosomeDataset(data_dir, window, length,val_chr,feature=feature,itype=itype,mode='val')
+# val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False,drop_last=True,num_workers=8)
+
 
 
 model=newmodel.ConvTransModel(False,16)
